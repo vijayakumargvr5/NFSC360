8f073032ccce448aa84e611c330b95b64e4c67d5
vijayakumargvr5 <152606613+vijayakumargvr5@users.noreply.github.com>
2023-12-12 18:27:20 +0530

    Update Deploy.sh

--- Deploy.sh
+++ Deploy.sh
@@ -1,2 +1,17 @@
  i have created shell script
+ Notepad++ v8.6 new features & bug-fixes:
+
+ 1. Multi-edit is fully supported in Notepad++.
+ 2. Make multi-select background & caret colours customizable.
+ 3. Make session inaccessible files remembered (empty & read-only document as placeholder).
+ 4. Fix missing session invalid error for user session & enhance API NPPM_GETNBSESSIONFILES.
+ 5. Fix network shared files saving regression.
+ 6. Update Scintilla to v5.3.8 & Lexilla to v5.2.8.
+ 7. Fix docking panel crash due to messing up config.xml.
+ 8. Fix invalid styler.xml making Notepad++ crash issue.
+ 9. Fix tab-closing crash by middle mouse button (unexpected mouse position).
+10. Fix 2 performance issues in Style Configurator.
+11. Add 3 line operation (delete, copy & cut) shortcuts.
+12. Display extra info in the status bar of Find/Replace dialog to avoid PEBKAC.
+13. Fix "Hide lines" command hiding unselected lines issue.
 
